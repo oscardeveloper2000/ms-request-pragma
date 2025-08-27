595f97cc663beb1161aff1efc9952794f0ca592f
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-08-27 14:13:24 -0500

    feat(service): integrate reactive-web, REST consumer, and R2DBC PostgreSQL modules with configurations and DTO mapping

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/request/dto/RequestApplicationRecord.java
@@ -0,0 +1,26 @@
+package co.com.bancolombia.api.request.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public record RequestApplicationRecord(
+        @NotBlank(message = "Document number is mandatory")
+        String documentNumber,
+        @NotNull(message = "Amount is mandatory")
+        @Positive(message = "Amount must be a positive number")
+        BigDecimal amount,
+        @NotNull(message = "Term is mandatory")
+        @Positive(message = "Term must be a positive number")
+        Integer term,
+        @NotBlank(message = "Email is mandatory")
+        @Email(message = "Email should be valid")
+        String email,
+        @NotNull(message = "Loan type ID is mandatory")
+        Long loanTypeId,
+        @NotNull(message = "status type ID is mandatory")
+        Long statusId
+) {
+}
+
