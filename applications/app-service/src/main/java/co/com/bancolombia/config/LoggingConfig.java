9c693cc020c6c0981d7462363e8a750638fbc135
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-08-27 14:10:07 -0500

    feat(config): add logging configuration with Slf4jLoggerAdapter and dependencies for reactive-web

---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/LoggingConfig.java
@@ -0,0 +1,15 @@
+package co.com.bancolombia.config;
+
+import co.com.bancolombia.model.common.LoggerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public LoggerPort loggerPort() {
+        return new Slf4jLoggerAdapter();
+    }
+}
+
