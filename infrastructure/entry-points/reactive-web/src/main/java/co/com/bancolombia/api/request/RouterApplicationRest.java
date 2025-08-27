38790afe5d39fec79b93e32db3b138c9165f32f3
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-08-27 14:09:18 -0500

    feat(adapter): add reactive REST API adapter with routing, handlers, configurations, and tests

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/request/RouterApplicationRest.java
@@ -0,0 +1,23 @@
+package co.com.bancolombia.api.request;
+
+import co.com.bancolombia.api.config.RequestAppPath;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouterApplicationRest {
+    private final RequestAppPath path;
+    private final RequestApplicationHandler handler;
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(RequestApplicationHandler handler) {
+        return route(POST(path.getRequest()), handler::listenPOSTUseCase);
+    }
+}
