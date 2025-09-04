df70d8708263cde1a54f57d6f3ff9837fea0d880
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-04 14:27:04 -0500

    feat(request): add applyFilterByStatus method to return CustomPageResponseReport for paginated request applications

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/requestapplication/RequestApplicationEvents.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/requestapplication/RequestApplicationEvents.java
@@ -1,8 +1,16 @@
 package co.com.bancolombia.usecase.requestapplication;
 
+import co.com.bancolombia.model.common.CustomPageResponseReport;
+import co.com.bancolombia.model.common.PageResponse;
+import co.com.bancolombia.model.requestapplication.PageRequest;
 import co.com.bancolombia.model.requestapplication.RequestApplication;
+import co.com.bancolombia.model.requestapplication.RequestReportResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+
 
 public interface RequestApplicationEvents {
     Mono<RequestApplication> applySave(RequestApplication requestApplication);
+    Mono<CustomPageResponseReport<RequestReportResponse>> applyFilterByStatus(PageRequest pageable, Long statusId, String token);
 }
