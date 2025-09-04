2b5c93a7b3e07735774276ee1648072ce8e03ed7
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-04 14:28:32 -0500

    feat(repository): add methods to find and count RequestApplications by statusId with pagination support

--- domain/model/src/main/java/co/com/bancolombia/model/requestapplication/gateways/RequestApplicationRepository.java
+++ domain/model/src/main/java/co/com/bancolombia/model/requestapplication/gateways/RequestApplicationRepository.java
@@ -1,8 +1,16 @@
 package co.com.bancolombia.model.requestapplication.gateways;
 
+import co.com.bancolombia.model.requestapplication.PageRequest;
 import co.com.bancolombia.model.requestapplication.RequestApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+
 
 public interface RequestApplicationRepository {
     Mono<RequestApplication> save(RequestApplication requestApplication);
+    Flux<RequestApplication> findAllByStatusIdWithPageable (Long statusId, PageRequest pageable);
+    Flux<RequestApplication> findAllByStatusId (Long statusId);
+    Mono<Long> countByStatusId (Long statusId);
+
 }
