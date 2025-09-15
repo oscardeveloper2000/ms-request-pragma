2c322b955dac13c8ad80061a9e25b38ca15e2f39
Oscar Sanabria <oscarsanabria@unisangil.edu.co>
2025-09-15 00:50:56 -0500

    feat(domain): add method to publish loan report messages in MessagePublisherGateway

--- domain/model/src/main/java/co/com/bancolombia/model/external/messaging/gateway/MessagePublisherGateway.java
+++ domain/model/src/main/java/co/com/bancolombia/model/external/messaging/gateway/MessagePublisherGateway.java
@@ -6,5 +6,6 @@ import reactor.core.publisher.Mono;
 public interface MessagePublisherGateway {
     Mono<Void> publishLoanCalculateCapacity(String message);
     <T> Mono<String> publishLoanNotificationEmail(T messageObject);
+    <T> Mono<String> publishReportLoan(T messageObject);
     // Futuras colas aquí sin romper implementaciones existentes
 }
